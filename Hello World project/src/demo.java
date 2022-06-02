e3a4a23b1aff0e5bf703b7d565489413fd6d6d47
Anshrutha <Anshrutha@LAPTOP-G6P4IQNB>
2022-06-02 21:59:39 +0530

    first commit

---
+++ Hello World project/src/demo.java
@@ -0,0 +1,11 @@
+
+public class demo {
+
+	public static void main(String args[]) {
+		String str = "Hello";
+		String str1 = "Hello";
+		String str2 = new String("Hello");
+		System.out.println(str == str1);
+		System.out.println(str1 == str2);
+		}
+	}
