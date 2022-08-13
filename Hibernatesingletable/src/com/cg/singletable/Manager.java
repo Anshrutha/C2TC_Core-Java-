8ad39d2f8377d24685f1566bd005394071059565
Anshrutha <Anshrutha@LAPTOP-G6P4IQNB>
2022-08-13 21:36:05 +0530

    first commit

---
+++ Hibernatesingletable/src/com/cg/singletable/Manager.java
@@ -0,0 +1,27 @@
+package com.cg.singletable;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("MGR")
+public class Manager extends Employee{
+	
+	
+	private static final long serialVersionUID = 1L;
+	private String DepartmentName;
+
+	public String getDepartmentName() {
+		return DepartmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		DepartmentName = departmentName;
+	}
+	
+	
+
+}
+
+
+
